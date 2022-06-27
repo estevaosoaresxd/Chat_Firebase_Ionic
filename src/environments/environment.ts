7c72262386a6901c42baee2d7638256c4b556d18
Estevão Soares <67278181+estevaosoaresxd@users.noreply.github.com>
2022-06-26 23:44:48 -0300

    chat firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCez5LEQhsfc20E1VSVTGcr4Dp5PXL-u1o',
+    authDomain: 'chat-ionic-67ffe.firebaseapp.com',
+    projectId: 'chat-ionic-67ffe',
+    storageBucket: 'chat-ionic-67ffe.appspot.com',
+    messagingSenderId: '370383193360',
+    appId: '1:370383193360:web:2eba64951f0f3661465195',
+    measurementId: 'G-80JM2LMCBB',
+  },
+};
